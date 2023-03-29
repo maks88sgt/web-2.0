15dc972e4eea77a4e5fe4ef03ab888074f0b9ef5
Maksim Drobyshev <maks88sgt@yandex.ru>
2023-03-29 09:42:59 +0300

    add mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
+import {CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
